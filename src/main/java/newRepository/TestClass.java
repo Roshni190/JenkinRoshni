4fda7d5f43fd33e132c65e34efdd2b4c65ef2d97
Roshni <Roshni@LAPTOP-4KFUHQH6>
2024-06-03 17:00:46 +0530

    commit to jenkin

---
+++ src/main/java/newRepository/TestClass.java
@@ -0,0 +1,14 @@
+package newRepository;
+
+public class TestClass {
+
+	public void display() {
+		System.out.println("To Print");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass pn1=new TestClass();
+		pn1.display();
+	}
+
+}
